1abfbb358927a759a26e8bf31a7f24864b23a01e
PolushkinDenis <tigras666@mail.ru>
2021-07-27 14:35:01 +0400

    form
    add some form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField id="Name" label="Ваше имя" />
+      <div>
+        <TextField id="Sourname" label="Фамилия" />
+      </div>
+      <div>
+        <TextField id="Tel" label="Телефон" />
+      </div>
+      <div>
+        <TextField id="About" label="О вас" />
+      </div>
+      <div>
+        <Button color="primary">Отправить</Button>
+      </div>
+
     </div>
   );
 }
